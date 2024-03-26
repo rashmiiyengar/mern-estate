61fc86cd97f4563f5515ad82f127cb02d27f3187
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-03-26 13:47:36 -0700

    update header and make avatar private

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+
+  const {currentUser} = useSelector(state=>state.user);
+
+
   return (
     <header className="bg-slate-200 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
@@ -36,10 +41,17 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/signin">
-            {" "}
+
+
+          <Link to="/profile">
+           {currentUser? (
+            <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="currentuser"/>
+           
+           ):
             <li className="text-slate-700 hover:underline">Sign In</li>
-          </Link>
+        }</Link>
+
+
         </ul>
       </div>
     </header>
