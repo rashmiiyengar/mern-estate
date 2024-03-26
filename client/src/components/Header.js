64f7ed0b0f1463c0aa3a01a99a2927390a1dc73a
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-03-25 18:12:50 -0700

    complete signin page function

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
+          <Link to="/signin">
             {" "}
             <li className="text-slate-700 hover:underline">Sign In</li>
           </Link>
