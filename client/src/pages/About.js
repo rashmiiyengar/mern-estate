c708e51914d9ebd77c5ee3f4a201ac4398a14dcf
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-03-27 00:45:35 -0700

    update user api

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const About = () => {
+
+  const [counter,setCounter] = useState(0);
+
+  const handleClick1=()=>{
+    setCounter(counter+1)
+  }
+  const handleClick2=()=>{
+    setCounter(counter-1)
+  }
   return (
-    <div>About</div>
+
+    <div className='flex flex-col max-w-lg'>
+      <div className='bg-slate-400 text-center rounded-full'>
+      {counter}
+      </div>
+      
+      <button className='bg-slate-700 text-white rounded-lg m-3 p-3 hover:opacity-90' onClick={handleClick1}>
+      Increment
+      </button>
+      
+      <button className='bg-slate-700 text-white rounded-lg m-3 p-3 hover:opacity-90' onClick={handleClick2}>
+        Deccrement
+      </button>
+      </div>
   )
 }
 
