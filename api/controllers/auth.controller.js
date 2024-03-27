4141c4560e3711b7f37fd81dd88b9eb6cc016b02
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-03-27 16:55:58 -0700

    signout functionality added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,6 +51,15 @@ export const signin = async (req, res, next) => {
   }
 };
 
+export const signout = async(req,res,next)=>{
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out successfully");
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -58,7 +67,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie("access_token", token, { httpOnly: true })
         .status(200)
         .json(rest);
     } else {
@@ -68,7 +77,7 @@ export const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          req.body.name.split(' ').join('').toLowerCase() +
+          req.body.name.split(" ").join("").toLowerCase() +
           Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
@@ -78,7 +87,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
       res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie("access_token", token, { httpOnly: true })
         .status(200)
         .json(rest);
     }
