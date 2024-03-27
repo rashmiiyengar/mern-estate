bffa00b8cd592966fcd44815fb93361180ba0722
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-03-27 16:35:42 -0700

    delete user api

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,7 +1,6 @@
 import { errorHandler } from "./error.js";
 import jwt from "jsonwebtoken";
 
-
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
   if (!token) {
