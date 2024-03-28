752c17ee8041fba2e4dc08f1a1db58a9fef3611c
Rashmi Soundar <rashmisoundar@Rashmis-Laptop.local>
2024-03-27 23:07:57 -0700

    create listing api route

---
+++ api/models/listing.model.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountedPrice: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: false,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: String,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+
+const Listing = mongoose.model('Listing',listingSchema);
+
+export default Listing;
